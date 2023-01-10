384d24c4c31a9534b35821e8262c0d6cc14ef3cb
pritish <pritish01234@gmail.com>
2023-01-10 14:13:34 +0530

    10-01-2023

--- Param.cpp
+++ Param.cpp
@@ -48,10 +48,10 @@ Param::Param() {
 	/* Algorithm parameters */
 	numTrainImagesPerEpoch = 8000;	// # of training images per epoch 
     numTrainImagesPerBatch = 1;   // # of training images per batch. It is 1 for SGD
-	totalNumEpochs = 125;	// Total number of epochs
+	totalNumEpochs = 12500;	// Total number of epochs
 	interNumEpochs = 1;		// Internal number of epochs (print out the results every interNumEpochs)
 	nInput = 400;     // # of neurons in input layer
-	nHide = 100;      // # of neurons in hidden layer
+	nHide = 128;      // # of neurons in hidden layer
 	nOutput = 10;     // # of neurons in output layer
 	alpha1 = 0.4;	// Learning rate for the weights from input to hidden layer
 	alpha2 = 0.2;	// Learning rate for the weights from hidden to output layer
